5b41ea5375f116d1f68a971e90e83df44117410f
Ahsan Ali <46025324+ahsanali2000@users.noreply.github.com>
2020-06-09 17:49:31 +0500

    template set

--- Water_management/database/models.py
+++ Water_management/database/models.py
@@ -1,25 +1,32 @@
 from django.db import models as md
 
-
-class Customer(md.Model):
+class Person(md.Model):
     name = md.CharField(max_length=100, blank=False, null=False)
     phoneNo = md.CharField(max_length=11, blank=False, null=False)
-    addresses = md.ManyToManyField("Address")
     cnic = md.CharField(max_length=13, blank=False, null=False)
+    username= md.CharField(max_length=50, unique=True)
+    password= md.CharField(max_length=50)
+
+class Customer(Person):
     noOfBottles = md.IntegerField(default=0)
     amountDue = md.IntegerField(default=0)
     approved = md.BooleanField(default=False)
     monthlyBill = md.IntegerField(default=0)
+    user_type= md.IntegerField(default=3, editable=False)
+
+class Employee(Person):
+    salary = md.IntegerField(default=0)
+    user_type= md.IntegerField(default=2, editable=False)
+
+class Admin(md.Model):
+    user_type= md.IntegerField(default=1, editable=False)
+    username= md.CharField(max_length=50, unique=True)
+    password= md.CharField(max_length=50)
 
 
 class Address(md.Model):
     address = md.TextField(default="")
-
-
-class Employee(md.Model):
-    name = md.CharField(max_length=100, blank=False, null=False)
-    phoneNo = md.CharField(max_length=11, blank=False, null=False)
-    cnic = md.CharField(max_length=13, null=False, blank=False)
+    person = md.ForeignKey(Person, on_delete=md.CASCADE)
 
 
 class Vehicle(md.Model):
@@ -29,11 +36,12 @@ class Vehicle(md.Model):
 
 class Order(md.Model):
     frequencyChoices = [(1, "One Time"), (2, "Recursive")]
+    delivered = md.BooleanField(default=False)
     customer = md.ForeignKey(Customer, on_delete=md.CASCADE)
     frequecy = md.CharField(max_length=10, choices=frequencyChoices, default=frequencyChoices[0], blank=False,
                             null=False)
     date = md.DateTimeField()
-
+    desc = md.CharField(max_length=100)#'a1:0, a2:0' this is formate for system
 
 class City(md.Model):
     city = md.CharField(max_length=100, null=False, blank=False)
@@ -47,3 +55,8 @@ class Area(md.Model):
 class Schedule(md.Model):
     day = md.CharField(max_length=10, null=False, blank=False)
     areas = md.ManyToManyField(Area)
+
+class Products(md.Model):
+    name = md.CharField(max_length=80, null=False, blank=False)
+    prince = md.IntegerField(null=False, blank=False)
+    code = md.CharField(max_length=2)
--- Water_management/water_management/settings.py
+++ Water_management/water_management/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'water_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
