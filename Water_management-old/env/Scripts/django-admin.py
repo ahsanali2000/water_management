66a8818accfd1be7cd2d1a248655586cc150028f
Ahsan Ali <46025324+ahsanali2000@users.noreply.github.com>
2020-06-15 23:39:28 +0500

    customer

--- Water_management-old/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\muham\onedrive\desktop\django~1\water_~1\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
